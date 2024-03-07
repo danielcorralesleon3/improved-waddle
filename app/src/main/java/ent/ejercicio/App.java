752b1a1195011ceab51b75d8041c0e26f47fd685
danielcorralesleon3 <dcorrales@nigul.cide.es>
2024-03-07 16:51:19 +0100

    subimos projecto a test

--- app/src/main/java/ent/ejercicio/App.java
+++ app/src/main/java/ent/ejercicio/App.java
@@ -5,7 +5,7 @@ package ent.ejercicio;
 
 public class App {
     public String getGreeting() {
-        return "Hello Worldd!";
+        return "Hello Worldasdd!";
     }
 
     public static void main(String[] args) {
